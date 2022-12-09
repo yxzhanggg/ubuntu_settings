72b3d98f6075dcd94a56dfe8b56eb228450d2fe6
Yaoxin Zhang <83542426+yxzhanggg@users.noreply.github.com>
2022-12-09 18:48:55 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,49 @@
-sudo apt update
-
-sudo apt install texlive-full
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 sudo apt-get install zsh
 chsh -s /bin/zsh
+
+sudo apt-get install python3-dev -y
+sudo apt-get install python3-numpy -y
+sudo apt-get install python3-pip -y
+
+pip install --upgrade pip -y
+pip install --upgrade pyside6 -y
+pip install --upgrade matplotlib -y
+pip install --upgrade numpy -y
+
+sudo apt install build-essential -y
+sudo apt-get install cmake -y
+sudo apt-get install git -y
+sudo apt-get install pkg-config -y
+sudo apt-get install libgtk-3-dev -y
+sudo apt-get install libavcodec-dev -y
+sudo apt-get install libavformat-dev -y
+sudo apt-get install libswscale-dev -y
+sudo apt-get install libv4l-dev -y
+sudo apt-get install libxvidcore-dev -y
+sudo apt-get install libx264-dev -y
+sudo apt-get install libjpeg-dev -y
+sudo apt-get install libpng-dev -y
+sudo apt-get install libtiff-dev -y
+sudo apt-get install gfortran -y
+sudo apt-get install openexr -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libtbb2 -y
+sudo apt-get install libtbb-dev -y
+sudo apt-get install libdc1394-dev -y
+sudo apt-get install libopenexr-dev -y
+sudo apt-get install libgstreamer-plugins-base1.0-dev -y
+sudo apt-get install libgstreamer1.0-dev -y
+sudo apt install texlive-full -y
+
+
 sudo apt install curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sudo apt install git
 git config --global user.name "yxzhanggg"
 git config --global user.email "yxzhangchn@gmail.com"
 ssh-keygen -t rsa
